5a57ca5848f9500570fbdffd97a6e3365b4a1ebb
shurrini <jesusrt2608@gmail.com>
2023-10-29 14:24:51 -0600

    Added possibility to add new nodes with their corresponding edges

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,27 @@ import ReactFlow, {
   Background,
   Node,
   Edge,
+  Position,
 } from 'reactflow';
 import 'reactflow/dist/style.css';
-
 import ButtonEdge from './ButtonEdge';
+import { Button } from "@/components/ui/button";
 
 //Defining InitialNodes properties
 const initialNodes: Node[] = [
   {
     id: 'button-1',
-    type: 'input',
     data: { label: 'Lugar 1' },
     position: { x: 0, y: 0 },
-    sourcePosition: 'right', //Position of the connection point for outgoing edges
+    sourcePosition: Position.Right, //Position of the connection point for outgoing edges
+    targetPosition: Position.Left, //Position of the connection point for incoming edges
   },
   { 
     id: 'button-2', 
     data: { label: 'Lugar 2' }, 
     position: { x: 300, y: 0 },
-    targetPosition: 'left', //Position of the connection point for incoming edges
+    sourcePosition: Position.Right,
+    targetPosition: Position.Left, 
   },
 ];
 
@@ -51,11 +53,44 @@ const edgeTypes = {
 
 //Main component definition
 const EdgesFlow = () => {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
+  const addNode = () => {
+    // Get the position of the last node
+    const lastNodePosition = nodes[nodes.length - 1].position;
+
+    // Create a new node
+    const newNode = {
+      id: `button-${nodes.length + 1}`,
+      data: { label: `Lugar ${nodes.length + 1}` },
+      position: { x: lastNodePosition.x + 300, y: lastNodePosition.y },
+      sourcePosition: Position.Right,
+      targetPosition: Position.Left,
+    };
+
+    // Create a new edge from the last node to the new node
+    const newEdge = {
+      id: `edge-${edges.length + 1}`,
+      source: nodes[nodes.length - 1].id,
+      target: newNode.id,
+      type: 'buttonedge',
+      data: { 
+        sourceLabel: nodes[nodes.length - 1].data.label,
+        targetLabel: newNode.data.label,
+      },
+    };
+
+    // Update the nodes and edges state
+    setNodes((ns) => [...ns, newNode]);
+    setEdges((es) => [...es, newEdge]);
+  };
+
   return (
-    <div style={{ height: height, width: width }}> {/*So the canvas fits the whole window*/}  
+    <div style={{ height: height, width: width}}> {/*So the canvas fits the whole window*/}  
+      <div style={{ position: 'absolute', bottom: 15, right: 15, zIndex: 1000}}>
+        <Button onClick={addNode}>Agregar Lugar</Button>
+      </div>
       <ReactFlow
         nodes={nodes}
         edges={edges}
